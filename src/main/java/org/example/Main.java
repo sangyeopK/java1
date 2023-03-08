cfd6d20bb3460d0393c05ac02129dddd7231d144
sangyeopK <creativethony@naver.com>
2023-03-08 12:54:56 +0900

    2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int age = 30;
+
+        System.out.println("당신의 나이 : " + age);
+
+        if (age >= 20 && age<=34) {
+            System.out.println("청년입니다");
+        }
+        else {
+            System.out.printf("X");
+        }
     }
 }
