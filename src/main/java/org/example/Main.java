083acafcfbd8ff2af508d35427def4a08cf29ed9
sangyeopK <creativethony@naver.com>
2023-03-13 13:27:12 +0900

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,15 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int age = 30;
 
-        System.out.println("당신의 나이 : " + age);
-
-        if (age >= 20 && age<=34) {
-            System.out.println("청년입니다");
-        }
-        else {
-            System.out.printf("X");
-        }
     }
 }
